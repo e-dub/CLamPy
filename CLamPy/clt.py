9756a469aee49696cbd252904ec4f00e3f0dc1a1
wehrle <erich.wehrle@unibz.it>
2020-07-14 23:17:12 +0200

    added comments

--- CLamPy/clt.py
+++ CLamPy/clt.py
@@ -17,19 +17,9 @@ class laminate(object):
 
     Attributes
     ----------
-    says_str : str
-        a formatted string to print out what the animal says
-    name : str
-        the name of the animal
-    sound : str
-        the sound that the animal makes
-    num_legs : int
-        the number of legs the animal has (default 4)
 
     Methods
     -------
-    says(sound=None)
-        Prints the animals name and what sound it makes
 
     Parameters
     ----------
