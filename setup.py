ad98fb4ac326235022349dd2d4af9a6684d63591
wehrle <Erich.Wehrle@unibz.it>
2022-01-05 20:25:10 +0100

    readme for pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import os
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
+
 def package_files(directory):
     paths = []
     for (path, directories, filenames) in os.walk(directory):
@@ -8,13 +14,19 @@ def package_files(directory):
             paths.append(os.path.join('..', path, filename))
     return paths
 
+
 if __name__ == '__main__':
     from distutils.core import setup
+
     extra_files = package_files('CLamPy')
-    setup(name='CLamPy',
-          version='1.1',
-          description='Classical LAMinate theory for the lightweight design of structures and systems in PYthon',
-          author='E. M. Gioia & E. J. Wehrle',
-          package_data={'': extra_files},
-          license='GNU Lesser General Public License',
-          packages=['CLamPy'])
+    setup(
+        name='CLamPy',
+        version='1.1',
+        description='Classical LAMinate theory for the lightweight design of structures and systems in PYthon',
+        author='E. M. Gioia & E. J. Wehrle',
+        package_data={'': extra_files},
+        license='GNU Lesser General Public License',
+        packages=['CLamPy'],
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+    )
